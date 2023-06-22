d6c1e26116fa3211d46b8753dce1c24eb7ff158a
JohnCru2010 <juancrualf@outlook.com>
2023-06-22 18:53:58 +0200

    test

--- tests/script.js
+++ tests/script.js
@@ -1,5 +1,5 @@
 function myFunction() {
     document.getElementById("text").innerHTML = "Hello World!";
     var zurullo = document.getElementById("button");
-    zurullo.value = "ALGO";
+    zurullo.value = "ALGOs";
 }
